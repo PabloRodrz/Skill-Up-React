444d86655c2a25b14ed2299f09cc5566734deb2d
Fede <78286672+federeinoso@users.noreply.github.com>
2022-11-03 17:54:24 -0300

    Add icons to assets (#3)

---
+++ src/assets/icons/Logo.jsx
@@ -0,0 +1,30 @@
+function Logo(props) {
+  return (
+    <svg
+      width={41}
+      height={30}
+      viewBox="0 0 41 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_207_1018)">
+        <path
+          d="M0.138733 29.6928L13.9855 0.286011H18.2471L32.0896 29.6928H27.9466L16.1142 4.55475L4.28176 29.6928H0.138733Z"
+          fill="white"
+        />
+        <path
+          d="M8.45032 29.6928L22.2928 0.286011H26.5544L40.3969 29.6843L36.2623 29.697L26.5205 9.00133L24.4215 4.55474L12.5891 29.6928H8.45032Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_207_1018">
+          <rect width={41} height={30} fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default Logo;
---
+++ src/assets/icons/LogoLoginIcon.jsx
@@ -0,0 +1,22 @@
+const LogoLoginIcon = (props) => {
+  return (
+    <svg
+      width={117}
+      height={84}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M.396 83.14L39.91.8h12.16l39.503 82.34H79.75L45.984 12.753 12.22 83.14H.396z"
+        fill="#52BAF1"
+      />
+      <path
+        d="M24.114 83.14L63.616.8h12.161l39.502 82.316-11.799.036-27.8-57.948-5.99-12.45L35.925 83.14h-11.81z"
+        fill="#52BAF1"
+      />
+    </svg>
+  );
+};
+
+export default LogoLoginIcon;
