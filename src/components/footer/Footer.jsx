ba7a85c0ee25a5886c3c3dbec6a884716948ec02
Francisco Garciarena <francisco.garciarena94@gmail.com>
2022-11-07 19:44:59 -0300

    footer created

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,48 @@
+// Icons
+import Logo from '../../assets/icons/FooterLogo'
+import {
+  FaInstagram,
+  FaTwitterSquare,
+  FaFacebookSquare,
+  FaYoutube,
+} from 'react-icons/fa'
+
+// Styles
+import styled from './footer.module.css'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className={styled.container}>
+      <footer className={styled.wrapper}>
+        <section className={styled.logo}>
+          <Logo />
+          <h2 className={styled.brand}>AlkyBank</h2>
+        </section>
+
+        <section className={styled.socialMedia}>
+          <h4>Follow us</h4>
+          <div className={styled.icons}>
+            <FaYoutube />
+            <FaInstagram />
+            <FaTwitterSquare />
+            <FaFacebookSquare />
+          </div>
+        </section>
+
+        <section className={styled.subscribe}>
+          <div>
+            <h4>To recieve more information subscribe.</h4>
+            <div className={styled.footerForm}>
+              <form>
+                <input placeholder='Email' type='email' />
+                <button>Subscribe</button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </footer>
+      <div className={styled.copyright}>Development Team R7</div>
+    </footer>
+  )
 }
 export default Footer
