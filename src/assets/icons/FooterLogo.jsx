faa05b53ae437b18bd7cb12e205703009cfcab79
Francisco Garciarena <80267392+Francisco-07@users.noreply.github.com>
2022-11-07 19:47:21 -0300

    Footer (#13)
    * persist user data, minor changes in redux auth
    
    * footer created

---
+++ src/assets/icons/FooterLogo.jsx
@@ -0,0 +1,22 @@
+const LogoLoginIcon = (props) => {
+  return (
+    <svg
+      width={74}
+      height={53}
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path
+        d='M.25 52.457L25.242.505h7.692l24.984 51.952H50.44L29.084 8.047 7.728 52.457H.25z'
+        fill='#fff'
+      />
+      <path
+        d='M15.252 52.457L40.236.505h7.691l24.984 51.937-7.462.023-17.583-36.563-3.788-7.855-21.356 44.41h-7.47z'
+        fill='#fff'
+      />
+    </svg>
+  )
+}
+
+export default LogoLoginIcon
