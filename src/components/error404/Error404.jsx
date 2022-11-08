cc40da3cf66a171917e2ee3c360be3a5b167fdbb
Federico <federeinoso01@gmail.com>
2022-11-07 21:54:55 -0300

    Add css module

--- src/components/error404/Error404.jsx
+++ src/components/error404/Error404.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
-import "./error404.css";
+import styled from "./error404.module.css";
 
 function Error404() {
   return (
     <>
-      <section className="errorPage">
-        <div className="container">
-          <div className="textContainer">
+      <section className={styled.errorPage}>
+        <div className={styled.container}>
+          <div className={styled.textContainer}>
             <h1>Ooops!</h1>
-            <p className="message">
+            <p className={styled.message}>
               We can't seem to find the page you're looking for.
             </p>
-            <span className="bold mr">Error 404.</span>
+            <span className={`${styled.bold} ${styled.mr}`}>Error 404.</span>
             <span>Not found</span>
 
-            <Link className="link bold" to={"/dashboard"}>
-              <p className="link-text">Go back to homepage</p>
+            <Link className={`${styled.bold} ${styled.link}`} to={"/dashboard"}>
+              <p className={styled.linkText}>Go back to homepage</p>
             </Link>
           </div>
         </div>
