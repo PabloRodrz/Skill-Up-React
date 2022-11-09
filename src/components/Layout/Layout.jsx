19aca7643b0b6baaba40887dc78e16fd1b37265f
Fede <78286672+federeinoso@users.noreply.github.com>
2022-11-09 15:55:48 -0300

    Send money page, layout component fixed (#23)
    * Send money page, layout component fixed
    
    * minor fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,14 @@
-import Navbar from "../Navbar/Navbar";
-import Title from "../title/Title"
-import styled from './Layout/Layout.module.css'
+import Navbar from '../Navbar/Navbar';
+import Title from '../title/Title';
+import styled from './Layout.module.css';
 
-const Layout = ({page, children}) => {
+const Layout = ({ page, children }) => {
   return (
-    <div className={ styled.layout }>
+    <div className={styled.layout}>
       <Navbar />
-      <div className={ styled.pageContainer }>
-      <Title text={page}/>
-      <div className={ styled.cardsContainer}>
-      {children}
-      </div>
+      <div className={styled.pageContainer}>
+        <Title text={page} />
+        <div className={styled.cardsContainer}>{children}</div>
       </div>
     </div>
   );
